d2c0c0028f191a26118f1057df7d4c5d257a8f33
PanosKikas <kikaspanagiotis@gmail.com>
2024-06-29 22:58:06 +0100

    Initial player and enemy setup

---
+++ Source/Aura/Private/Character/CharacterBase.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Character/CharacterBase.h"
+
+ACharacterBase::ACharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::Type::NoCollision);
+}
+
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
---
+++ Source/Aura/Public/Character/EnemyCharacter.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CharacterBase.h"
+#include "Interaction/HighlightInterface.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AEnemyCharacter : public ACharacterBase, public IHighlightInterface
+{
+	GENERATED_BODY()
+public:
+	AEnemyCharacter();
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
+};
