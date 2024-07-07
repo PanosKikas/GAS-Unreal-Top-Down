46a580f8c8b4664dc487a467ea6b0a12b4f3a602
PanosKikas <kikaspanagiotis@gmail.com>
2024-07-07 19:28:38 +0100

    Instant and Duration Gameplay Effects + Riderlink move

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -9,7 +9,7 @@ UAuraAttributeSet::UAuraAttributeSet()
 	InitMaxHealth(100.f);
 	InitHealth(50.f);
 	InitMaxMana(100.f);
-	InitMana(50.f);
+	InitMana(10.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
